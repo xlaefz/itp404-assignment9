e50fc524ad483ad80ed4fe49045281002cea3232
Jason <jasonzhe@usc.edu>
2016-11-22 19:48:18 -0800

    done

--- api/yelp.js
+++ api/yelp.js
@@ -2,25 +2,12 @@ var dotenv = require('dotenv');
 dotenv.config();
 var Yelp = require('yelp');
 
-// var yelp = new Yelp({
-//   consumer_key: 'zwjGaVRVWnpFhiXaDBoqYw',//process.env.consumer_key,
-//   consumer_secret: '1qUMkZ7YHTfyza1MlAEqvWRkdXA',//process.env.consumer_secret,
-//   token: 'HQo3rovV3v5z2JzKS2cMW-DepfEcIRof',//process.env.token,
-//   token_secret: '80ptjRiqxBIl0sdXAlEY_l9cAYM',//process.env.token_secret,
-// });
-
 var yelp = new Yelp({
   consumer_key:process.env.consumer_key,
   consumer_secret:process.env.consumer_secret,
   token:process.env.token,
   token_secret:process.env.token_secret
 });
-// var yelp = new Yelp({
-//   consumer_key:process.env.consumer_key,
-//   consumer_secret:process.env.consumer_secret,
-//   token:process.env.token,
-//   token_secret:process.env.token_secret,
-// });
 
 module.exports = {
   search: function(searchOptions) {
