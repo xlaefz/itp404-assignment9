9697791787746370ff65f5089781a80d50a79e2f
Jason <jasonzhe@usc.edu>
2016-11-22 19:45:46 -0800

    done

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
-require('dotenv').config();
-var http = require('http');
-var express = require('express');
-var app = express();
-// Every call to `ig.use()` overrides the `client_id/client_secret`
-// or `access_token` previously entered if they exist.
-var consumer_token = process.env.consumer_token;
-var consumer_secret = process.env.consumer_secret;
-var consumer_key = process.env.consumer_key;
+var dotenv = require('dotenv');
+dotenv.config();
+var express = require('express')
+var app = express()
+var yelp = require('./api/yelp.js');
 
-
-// var ig = require('instagram-node').instagram();
-// ig.use({ access_token: consumer_token });
-// ig.use({ client_id: consumer_key,
-//          client_secret: consumer_secret});
-
-app.get('/results', function(request, response){
-import Instagram from 'node-instagram';
-  const instagram = new Instagram({
-    clientId: process.env.consumer_key,
-    accessToken: process.env.consumer_token,
+app.get('/results', function(request, response) {
+  yelp.search({ term: 'pizza', location: 'montreal'}).then(function(results) {
+    response.json(results);
+  }, function(err) {
+    response.json(err);
   });
-  instagram.get('users/self').then((data) => {
-    console.log("hi");
-    response.json(data);
-  });
-  instagram.get('/users/self/media/recent').then((data) =>{
-    response.json(data);
-  })
-})
+});
 
-app.listen(3000);
+app.listen(3000)
